4f59955b6ea2d72ce1413b9340cd71a261064bdd
GhifariFR <ghifarifr037@gmail.com>
2024-05-28 16:26:31 +0700

    fix model error

--- application/models/Model_buku.php
+++ application/models/Model_buku.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class ModelBuku extends CI_Model
+class Model_buku extends CI_Model
 {
     //manajemen buku
     public function getBuku()
